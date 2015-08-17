dd971d411db4a9562d273e2c6a148981aadb45f1
Mark Needham <m.h.needham@gmail.com>
2015-08-17 23:27:01 +0100

    script to download the data

---
+++ download.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+wget -nc http://data.dft.gov.uk/road-accidents-safety-data/Stats19-Data1979-2004.zip
+wget -nc http://data.dft.gov.uk.s3.amazonaws.com/road-accidents-safety-data/Stats19_Data_2005-2014.zip
